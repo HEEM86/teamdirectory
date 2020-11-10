0c37377d4842f8cedb9d555fda115d38bac6dd14
Slice Wess <ernestwesson86@gmail.com>
2020-11-09 20:58:21 -0800

    finished more functionality and readme and demo video

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# teamdirectory
+## Team Directory Tracker
+
+
+**Description**
+
+The Team Directory Tracker allows the user to go into the database to view, add or make changes. 
+
+Features:
+
+- User has the ability to View all Departments and Employees
+- User has the ability to Vill all of the active Employees
+- User has the ability to make changes to database: add/remove
+- User can view the managers in the database
+
+
+## The Finished Product 
+
+
+[Database Work Video Link](https://youtu.be/jAVGsukErWk)
+
+
+
+## Challenges :loudspeaker:
+
+
+1. Wasn't able to get the remove employee selection, I believe the problem is with the query selector syntax using "DELETE" did some trial an error couldn't quite get it.
+
+[Remove Employee Challenge](https://youtu.be/jAVGsukErWk)
+
+2. The view manager selection gave me an issue, because the manager had a FOREIGNKEY because the manager is an employee but not group with the other employees.
+
+
+## Author :zap:
+
+Ernest Wesson 
+
+
+## Credits :star:
+
+Slack: Study Group #3
+
+Lofi Music: https://www.youtube.com/watch?v=tutZKLeGrCs
+
+
+## License :page_with_curl:
+
+
+MIT License
+
+Copyright (c) [2020] [Ernest]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+https://choosealicense.com/licenses/mit/
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ connection.connect(function (err) {
     return;
   }
   console.log("connected as id " + connection.threadId);
-  viewDepartments();
+  // viewDepartments();
   viewEmployees();
   setTimeout(beginQuestions, 1000);
 });
@@ -18,9 +18,12 @@ connection.connect(function (err) {
 const teamMembers = [];
 
 function viewDepartments() {
+  console.log("hello")
   connection.query("SELECT * FROM department", function (err, res, fields) {
+    console.log(res)
     if (err) throw err;
     console.table(res);
+    // beginQuestions();
   });
 }
 
@@ -28,6 +31,7 @@ function viewEmployees() {
   connection.query("SELECT * FROM employee", function (err, res, fields) {
     if (err) throw err;
     console.table(res);
+    // beginQuestions();
   });
 }
 
@@ -44,7 +48,7 @@ function beginQuestions() {
           "Add Employee",
           "View all Employees",
           "Remove Employee",
-          "Add a role",
+          "Add a Role",
           "View Manager"
         ],
       },
@@ -52,7 +56,8 @@ function beginQuestions() {
     .then(function (choice) {
       console.log("choice is: ");
       console.log(choice);
-      if (choice.questions === "View all department") {
+      if (choice.questions === "View all departments") {
+        console.log("test")
         viewDepartments();
       } else if (choice.questions === "Add a department") {
         addDepartment();
@@ -62,6 +67,8 @@ function beginQuestions() {
         viewEmployees();
       } else if (choice.questions === "Remove Employee") {
         removeEmployee();
+      } else if (choice.questions === "Add a Role") {
+        addRole();
       } else if (choice.questions === "View Manager") {
         viewManager();
       } else {
@@ -81,7 +88,7 @@ function addDepartment() {
       console.log("answer is: ");
       console.log(answer);
       var deptName = answer.dept_name;
-      console.log(deptName + " has been added!\n");
+      console.log("Stay with us, " + deptName + " is being added!\n");
       var query = connection.query(
         "INSERT INTO department SET ?",
         {
@@ -89,8 +96,164 @@ function addDepartment() {
         },
         function (err, res) {
           if (err) throw err;
-          console.log(res);
+          console.log(deptName + " department(s) was added!\n");
         }
-      );
-    });
+      )
+      //console.log(query.sql);
+    })
+    .then (function() {
+      setTimeout(beginQuestions, 1000);
+    })
+  
+    
 }
+
+
+
+function addRole()  {
+  inquirer
+  .prompt([
+    {
+      name: "title",
+      type: "input",
+      message: "What is the name of the role you want to add?"
+    },
+    {
+      name: "salary",
+      type: "input",
+      message: "What is the salary for this role?" 
+    },
+    {
+      name: "department_id",
+      type: "input",
+      message: "What department id do you want to add? Choose a number betwween 1-9."  
+    }
+  
+  ])
+  .then(function (role) {
+    console.log("the role is: ");
+    // console.log(role);
+    // console.log(role.title);
+    // console.log(role.salary);
+    // console.log(role.department_id);
+    var roleName = role.title;
+    console.log("Wait for it... " + roleName + " is now being added!\n");
+    var query = connection.query(
+    "INSERT INTO role SET ?",
+    {
+      title: role.title,
+      salary: role.salary,
+      department_id: role.department_id
+
+    },
+    function (err, res) {
+        if (err) throw err;
+         console.log(roleName + " role(s) added!\n");
+       }
+   )
+  console.log(query.sql);
+  })
+  .then (function() {
+    setTimeout(beginQuestions, 1000);
+  })
+}
+
+
+  function addEmployee()  {
+    inquirer
+    .prompt([
+      {
+        name: "first_name",
+        type: "input",
+        message: "What is the first name of the employee?"
+      },
+      {
+        name: "last_name",
+        type: "input",
+        message: "What is the last name of the employee?" 
+      },
+      {
+        name: "role_id",
+        type: "input",
+        message: "What is the role id of the new employee. Choose between 1-9."  
+      }
+    ])
+    .then(function (worker) {
+      console.log("the employee is: ");
+      var workerName = worker.first_name;
+      console.log("Wait for it... " + workerName + " is now being added!\n");
+      var query = connection.query(
+      "INSERT INTO employee SET ?",
+      {
+        first_name: worker.first_name,
+        last_name: worker.last_name,
+        role_id: worker.role_id
+  
+      },
+      function (err, res) {
+          if (err) throw err;
+           console.log(workerName + " role(s) added!\n");
+         }
+     )
+    console.log(query.sql);
+    })
+    .then (function() {
+      setTimeout(beginQuestions, 1000);
+    })
+  };
+
+
+  function removeEmployee()  {
+    inquirer
+    .prompt([
+      {
+        name: "first_name",
+        type: "input",
+        message: "What is the first name of the employee you want to remove?"
+      },
+      {
+        name: "last_name",
+        type: "input",
+        message: "What is the last name of the employee you want to remove?" 
+      }
+      // {
+      //   name: "role_id",
+      //   type: "input",
+      //   message: "What is the role id of the removed employee. Choose between 1-9."  
+      // }
+    ])
+    .then(function (remove) {
+      console.log("the employee is: ");
+      var removeName = remove.first_name;
+      console.log("Wait for it... " + removeName + " is now being removed!\n");
+      var query = connection.query(
+      "DELETE FROM employee WHERE role ?",
+      {
+        first_name: remove.first_name,
+        last_name: remove.last_name
+        // role_id: remove.role_id
+  
+      },
+      function (err, res) {
+          if (err) throw err;
+           console.log(removeName + " role(s) added!\n");
+         }
+     )
+    console.log(query.sql);
+    })
+    .then (function() {
+      setTimeout(beginQuestions, 1000);
+    })
+  };
+
+    // For inputs that are numbers use inquire validation. Inquirer documentation on npm. 
+
+    // use a basic input or create a function that quieres the database and grabs all of the department names and their ids and create a set of choices the allows the user to select a department without looking up the ID
+  
+  
+
+
+
+
+
+ 
